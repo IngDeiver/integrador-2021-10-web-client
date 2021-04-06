b0990a0a054bc4385f5ed668f5e04a5f825e9e40
Deiver Carrascal <ingendeiver@gmail.com>
2021-04-06 10:01:58 -0500

    fix: git

---
+++ src/pages/sync.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import '../styles/sync.css'
+import isElectron from 'is-electron';
+import WithMessage from '../hocs/withMessage';
+import WithAppLayout from '../layouts/appLayout'
+
+
+
+const Sync = () => {
+
+    useEffect(() => {
+        console.log("isElectron.", isElectron());
+    });
+
+    return (
+        <>
+            {
+                isElectron() ? (
+                <div className ="d-flex flex-column justify-content-center align-items-center p-5">
+                        <h1 className ="text-center text-muted my-2">Select a folder to sync</h1>
+                        <button className="btn btn-info"><i className="fas fa-sync"></i> Browser</button>
+                </div>):(
+                    <div className ="d-flex flex-column justify-content-center align-items-center p-5">
+                        <h1 className ="text-center my-2">Streams for labs</h1>
+                        <h3 style={{ marginRight:150, marginLeft:150}} className ="text-center text-muted">To keep your files synchronized, 
+                        download the desktop application available for Linux.</h3>
+                        <a download href={`${process.env.REACT_APP_GATEWAY_SERVICE_BASE_URL}/streams-for-lab_0.2.0_amd64.deb`} className="btn btn-success">
+                            <i className="fas fa-cloud-download-alt mr-2"></i> Download</a>
+                    </div>
+                )
+            }
+        </>
+    )
+}
+
+export default WithMessage(WithAppLayout(Sync))
---
+++ src/services/videoApiService.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import { getLocalSesion } from '../util/auth'
+
+
+const getAxiosInstance = () => {
+    return axios.create({
+        baseURL: `${process.env.REACT_APP_GATEWAY_SERVICE_BASE_URL}/api`
+    });
+}
+
+export const listVideos = async () => {
+    const { token } = await getLocalSesion();
+    const axiosInstance = await getAxiosInstance()
+    return axiosInstance.get(`/video`,
+        { headers: { 'Authorization': `Bearer ${token}`, 'Content-Type': 'application/json' } })}
+
+
+
+export const downloadVideo = async (videoId) => {
+    const { token } = await  getLocalSesion();
+    const axiosInstance = await getAxiosInstance()
+    return axiosInstance.get(`/video/download/${videoId}`, 
+        { headers: {'Authorization': `Bearer ${token}` },
+        responseType: 'blob'
+    })
+}
+export const shareVideo = async (videoId) => {
+    const { token } = await  getLocalSesion();
+    const axiosInstance = await getAxiosInstance()
+    return axiosInstance.get(`/video/share/${videoId}`, 
+        { headers: {'Authorization': `Bearer ${token}` },
+        responseType: 'blob'
+    })
+}
+
+
+export const removeVideos = async (videos) => {
+    const { token } = await  getLocalSesion();
+    const axiosInstance = await getAxiosInstance()
+    return axiosInstance.delete('/video',
+    { data: {files: videos},
+      headers: {'Authorization': `Bearer ${token}`, 'Content-Type' : 'application/json' }
+    })
+}
+
