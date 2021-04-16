2994bb9f0c17b332848fff96dc1cd8aabfd991e6
Deiver Carrascal <ingendeiver@gmail.com>
2021-04-16 15:19:41 -0500

    fix: share files

--- src/components/share.jsx
+++ src/components/share.jsx
@@ -1,70 +1,77 @@
-import React, { useState, useEffect , useRef} from 'react';
-import { getUsuarios } from '../services/fileApiService'
-import { Typeahead } from 'react-bootstrap-typeahead';
+import React, { useState, useEffect, useRef, useContext } from "react";
+import { getUsuarios } from "../services/fileApiService";
+import { Typeahead } from "react-bootstrap-typeahead";
 import { shareFile } from "../services/fileApiService";
 import { sharePhoto } from "../services/photoApiService";
 import { shareVideo } from "../services/videoApiService";
 import WithMessage from "../hocs/withMessage";
+import { AppContext } from "../context/AppProvider";
 
-
-const Modal = ({ showMessage,file }) => {
-
-  const refContainer = useRef();  
-  const [users, setUsers] = useState([])
+const Modal = ({ showMessage, file }) => {
+  const refContainer = useRef();
+  const [users, setUsers] = useState([]);
   const [singleSelections, setSingleSelections] = useState([]);
-
+  const context = useContext(AppContext);
+  const user = context[3];
   const listUsuarios = () => {
     getUsuarios().then((res) => {
-      const listusers = res.data
-      setUsers(listusers)
-    })
-  }
+      const listusers = res.data.filter(us => us.username.replace(/\s/g, "-").toLowerCase() !== user.username);
+      setUsers(listusers);
+    });
+  };
 
   const resetTypeAhead = () => {
-    setSingleSelections([])
-  }
+    setSingleSelections([]);
+  };
 
   useEffect(() => {
-    listUsuarios()
-  }, [file])
+    listUsuarios();
+  }, [file]);
 
   const onShare = (file, user) => {
-   
-    if (!file.shared_users.includes(user._id)){
-    if (file.type == "file") {
-      shareFile(file._id, user._id)
-        .then((res) => {
-          if (res.data){
-            
-            resetTypeAhead()
-            showMessage("File shared!");}
-        })
-        .catch((err) => showMessage(err.message, "error"));
-    } else if (file.type == "photo") {
-      sharePhoto(file._id, user._id)
-        .then((res) => {
-          if (res.data){
-            resetTypeAhead()
-            showMessage("Photo shared!");}
-        })
-        .catch((err) => showMessage(err.message, "error"));
+    if (!file.shared_users.includes(user._id)) {
+      if (file.type == "file") {
+        shareFile(file._id, user._id)
+          .then((res) => {
+            if (res.data) {
+              resetTypeAhead();
+              showMessage("File shared!");
+            }
+          })
+          .catch((err) => showMessage(err.message, "error"));
+      } else if (file.type == "photo") {
+        sharePhoto(file._id, user._id)
+          .then((res) => {
+            if (res.data) {
+              resetTypeAhead();
+              showMessage("Photo shared!");
+            }
+          })
+          .catch((err) => showMessage(err.message, "error"));
+      } else {
+        shareVideo(file._id, user._id)
+          .then((res) => {
+            if (res.data) {
+              resetTypeAhead();
+              showMessage("Video shared!");
+            }
+          })
+          .catch((err) => showMessage(err.message, "error"));
+      }
     } else {
-      shareVideo(file._id, user._id)
-        .then((res) => {
-          if (res.data){
-            
-            resetTypeAhead()
-            showMessage("Video shared!");}
-        })
-        .catch((err) => showMessage(err.message, "error"));
-    }}
-    else{
-      showMessage("User already has file shared", "warning")
+      showMessage("User already has file shared", "warning");
     }
-  }
+  };
 
   return (
-    <div className="modal" id="ventanaModalShared" tabindex="-1" role="dialog" aria-labelledby="tituloVentana" aria-hidden="true">
+    <div
+      className="modal"
+      id="ventanaModalShared"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="tituloVentana"
+      aria-hidden="true"
+    >
       <div className="modal-dialog" role="document">
         <div className="modal-content">
           <div className="modal-header">
@@ -73,7 +80,9 @@ const Modal = ({ showMessage,file }) => {
           <div className="modal-body">
             <div className="input-group">
               <div className="input-group-pretend mr-1">
-                <span className="input-group-text"><i style={{ fontSize: 20 }} className="fas fa-user-plus"></i></span>
+                <span className="input-group-text">
+                  <i style={{ fontSize: 20 }} className="fas fa-user-plus"></i>
+                </span>
               </div>
 
               <Typeahead
@@ -83,32 +92,39 @@ const Modal = ({ showMessage,file }) => {
                 options={users}
                 placeholder="Select users..."
                 selected={singleSelections}
-                ref = {refContainer}
+                ref={refContainer}
               />
             </div>
           </div>
           <div className="modal-footer">
-            <button className="btn btn-warning" type="button" data-dismiss="modal" onClick={
-              () => {
-                refContainer.current.clear()
-                setSingleSelections([])
-              }
-              }>
+            <button
+              className="btn btn-warning"
+              type="button"
+              data-dismiss="modal"
+              onClick={() => {
+                refContainer.current.clear();
+                setSingleSelections([]);
+              }}
+            >
               Close
-                                </button>
+            </button>
 
-            <button disabled={singleSelections.length == 0} className="btn btn-success" arial-label="Compartir" data-dismiss="modal"
-              onClick={() => onShare(file, singleSelections[0], refContainer.current.clear() )}>
+            <button
+              disabled={singleSelections.length == 0}
+              className="btn btn-success"
+              arial-label="Compartir"
+              data-dismiss="modal"
+              onClick={() =>
+                onShare(file, singleSelections[0], refContainer.current.clear())
+              }
+            >
               Share
-                                </button>
+            </button>
           </div>
-
         </div>
       </div>
     </div>
-  )
+  );
+};
 
-}
-
-
-export default WithMessage(Modal)
+export default WithMessage(Modal);
--- src/hocs/withMessage.jsx
+++ src/hocs/withMessage.jsx
@@ -3,13 +3,9 @@ import { ToastContainer, toast } from "react-toastify";
 
 // Hight Order Component (HOC) for extends common methods  like showMessage
 const WithMessage = (Component) => {
-  var msg = ""
   return class extends react.Component {
     showMessage = (message, type = "info") => {
-     if(message !== msg){
       toast(message, {type });
-      msg = message
-     }
     };
 
     render() {
